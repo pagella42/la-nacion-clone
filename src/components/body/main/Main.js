6d826cb3b28b72cd7e44e4a69414226ca2cbc9a6
pagella42 <pagella42@gmail.com>
2019-12-19 09:05:18 -0300

    fix

--- src/components/body/main/Main.js
+++ src/components/body/main/Main.js
@@ -8,18 +8,18 @@ import '../body.css'
 
 function Main(props) {
     const dates = {
-        "01":"Enero",
-        "02":"Febrero",
-        "03":"Marzo",
-        "04":"Abril",
-        "05":"Mayo",
-        "06":"Junio",
-        "07":"Julio",
-        "08":"Agosto",
-        "09":"Septiembre",
-        "10":"Octubre",
-        "11":"Noviembre",
-        "12":"Diciembre",
+        "01": "Enero",
+        "02": "Febrero",
+        "03": "Marzo",
+        "04": "Abril",
+        "05": "Mayo",
+        "06": "Junio",
+        "07": "Julio",
+        "08": "Agosto",
+        "09": "Septiembre",
+        "10": "Octubre",
+        "11": "Noviembre",
+        "12": "Diciembre",
 
     }
     return (
@@ -31,31 +31,36 @@ function Main(props) {
 
             <div id="tag-cont">
                 {props.tags ?
-                    props.tags.map((t, i) => i < 10 ?  <a href={`https://www.lanacion.com.ar/tema/${props.tagSlug[t]}`}><span class="tag">{t} •</span></a>: null) :
+                    props.tags.map((t, i) => i < 10 ? <a href={`https://www.lanacion.com.ar/tema/${props.tagSlug[t]}`}><span class="tag">{t} •</span></a> : null) :
                     null
                 }
                 {props.tags ?
                     props.tags.map((t, i) => i == 10 ? <a href={`https://www.lanacion.com.ar/tema/${props.tagSlug[t]}`}><span class="tag">{t}</span></a> : null) :
                     null}
             </div>
+            <div>
+                <div id="articles">
+                    {props.articles ?
+                        props.articles.map(a => a.subtype == 7 ?
+                            <div class="article">
+                                <a href={`https://www.lanacion.com.ar${a.website_url}`}>
+                                    <div class="article-photo-cont">
+                                        <img class="article-photo" src={a.promo_items.basic.url} />
+                                    </div>
+                                </a>
 
-            <div id="articles">
-                {props.articles ?
-                    props.articles.map(a => a.subtype == 7 ?
-                        <div class="article">
-                            <a href={`https://www.lanacion.com.ar${a.website_url}`}>
-                              <div class="article-photo-cont">
-                                <img class="article-photo" src={a.promo_items.basic.url} />
-                            </div>  
-                            </a>
-                            
-                            <div class="article-title-cont">
-                                <span class="article-title">{a.headlines.basic}</span>
-                                <span class="article-date">{ a.display_date.split("-")[2].split("T")[0] +" de " + dates[a.display_date.split("-")[1]] +" de " + a.display_date.split("-")[0]}</span>
-                            </div>
-                        </div> :
-                        null) :
-                    null}
+                                <div class="article-title-cont">
+                                    <span class="article-title">{a.headlines.basic}</span>
+                                    <span class="article-date">{a.display_date.split("-")[2].split("T")[0] + " de " + dates[a.display_date.split("-")[1]] + " de " + a.display_date.split("-")[0]}</span>
+                                </div>
+                            </div> :
+                            null) :
+                        null}
+                </div>
+                <div id="loadmore-cont">
+               {props.articles.length > 1 ? <button id="loadmore">MÁS NOTAS DE ACUMULADO GRILLA</button>: null} 
+
+                </div>
             </div>
         </div>
 
